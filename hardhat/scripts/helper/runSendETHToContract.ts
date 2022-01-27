55d577f377c6b532bf6ec80a04816c46d6cdca0d
HS <hiroaki.suzuki.714@gmail.com>
2022-01-27 11:13:16 +0000

    small change

--- hardhat/scripts/helper/runSendETHToContract.ts
+++ hardhat/scripts/helper/runSendETHToContract.ts
@@ -2,8 +2,7 @@ import { ethers as hardhatEthers } from "hardhat";
 import { utils } from 'ethers';
 
 const AMOUNT_WITHOUT_DECIMALS = "0.1";
-// const RECIPIENT = "0x8c5bF4D7216b1e18CC9172595FC46e831718fd76";
-const RECIPIENT = "0x08d8Aa61255d7C65e445378e4753bC90317cb8C8";
+const RECIPIENT = "..."; // Specify address
 
 async function main() {
     const [ owner ] = await hardhatEthers.getSigners();
